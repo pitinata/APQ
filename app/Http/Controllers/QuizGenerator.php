3af90702a84c02171131c83084c381d8fbbfcb9c
Pitinat A <pitinat@gmail.com>
2021-05-12 20:55:01 +0700

    Create basic function for question generator

---
+++ app/Http/Controllers/QuizGenerator.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Quiz;
+use Illuminate\Http\Request;
+
+class QuizGenerator extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+
+
+    public static function generateNumber($totalNumber, $digitPerNumber, $isMixDigit){
+
+        $numberArr = [];
+
+        for($i = 0; $i<$totalNumber; $i++){
+            if($isMixDigit===true){
+                array_push($numberArr, rand(0,pow(10,$digitPerNumber)-1));
+            }
+            else{
+                array_push($numberArr, rand(pow(10,$digitPerNumber-1),pow(10,$digitPerNumber)-1));
+            }
+        }
+
+        return $numberArr;
+    }
+
+    public static function generateOperator($totalNumber, $operator){
+
+        $operatorArr = [];
+
+        for($i = 0; $i<$totalNumber-1; $i++){
+            $thisOperator = match($operator[array_rand($operator)]){
+                "1"=>"+",
+                "2"=>"-",
+                "3"=>"*",
+                "4"=>"/",
+            };
+
+            array_push($operatorArr, $thisOperator);
+        }
+
+        return $operatorArr;
+    }
+
+
+
+    public static function calculateAnswer($numberArr, $operatorArr){
+
+        $tempNumberArr = [];
+        $tempOperatorArr = [];
+        $tempNumber = null;
+
+        while(count($operatorArr)>0){
+            $currentOperator = array_shift($operatorArr);
+
+            if($currentOperator == "*" || $currentOperator == "/"){
+                if($tempNumber==null){
+                    $tempNumber = array_shift($numberArr);
+                }
+
+                $currentNumber = array_shift($numberArr);
+                switch($currentOperator){
+                    case "*": $tempNumber = $tempNumber*$currentNumber; break;
+                    case "/": $tempNumber = $tempNumber/$currentNumber; break;
+                }
+            }
+            else{
+                if($tempNumber!=null){
+                    array_push($tempNumberArr, $tempNumber);
+                    $tempNumber=null;
+                }
+                else{
+                    array_push($tempNumberArr, array_shift($numberArr));
+                }
+                array_push($tempOperatorArr, $currentOperator);
+            }
+        }
+
+        if(count($numberArr)>0){
+            array_push($tempNumberArr, array_shift($numberArr));
+        }
+        if($tempNumber!=null){
+            array_push($tempNumberArr, $tempNumber);
+            $tempNumber=null;
+        }
+
+        $answer = $tempNumberArr[0];
+
+        for($i = 0; $i < count($tempOperatorArr); $i++){
+            switch($tempOperatorArr[$i]){
+                case "+": $answer += $tempNumberArr[$i+1]; break;
+                case "-": $answer -= $tempNumberArr[$i+1]; break;
+            }
+        }
+        return $answer;
+    }
+
+
+
+}
---
+++ tests/Unit/QuizGeneratorTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Http\Controllers\QuizGenerator;
+use Tests\TestCase;
+
+class QuizGeneratorTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+
+
+    public function test_number_generator(){
+
+        $totalNumber = 10;
+
+        $data = QuizGenerator::generateNumber($totalNumber, 1, false);
+
+        $this->assertIsArray($data);
+        $this->assertCount($totalNumber,$data);
+    }
+
+    public function test_operator_generator(){
+
+        $totalNumber = 10;
+
+        $data = QuizGenerator::generateOperator($totalNumber, ["1","2"]);
+
+        $this->assertIsArray($data);
+        $this->assertCount($totalNumber-1,$data);
+    }
+
+    public function test_answer_calculation(){
+
+        $answer = QuizGenerator::calculateAnswer([1,2,3,4,5], ["*","+","+","*"]);
+
+        $this->assertEquals(3, $answer);
+    }
+}
