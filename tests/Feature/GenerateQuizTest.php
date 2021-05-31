3111d2914e5d52246d28a39ee0fbf4f76621b1a5
Pitinat A <pitinat@gmail.com>
2021-05-31 21:02:55 +0700

    Add test for quiz paper generator page

---
+++ tests/Feature/GenerateQuizTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class GenerateQuizTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function test_guest_cannot_access_quiz_form_page()
+    {
+        $response = $this->get('/quiz/form');
+        $response->assertRedirect('/login');
+    }
+
+    public function test_user_can_access_quiz_form_page()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->get('/quiz/form');
+        $response->assertStatus(200);
+    }
+
+    public function test_guest_cannot_generate_quiz()
+    {
+        $response = $this->post('/quiz/form', [
+            "totalQuestion" => 10,
+            "totalNumber" => 2,
+            "digitPerNumber" => 2,
+            "isMixDigit" => true,
+            "operator" => ["+"]
+        ]);
+        $response->assertRedirect('/login');
+    }
+
+    public function test_user_can_generate_quiz()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->post('/quiz/form', [
+            "totalQuestion" => 10,
+            "totalNumber" => 2,
+            "digitPerNumber" => 2,
+            "isMixDigit" => true,
+            "operator" => ["+"]
+        ]);
+        $response->assertSee("Generate Successful");
+        $response->assertStatus(200);
+        $response->assertSessionHasNoErrors();
+    }
+}
