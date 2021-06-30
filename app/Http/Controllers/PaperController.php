d46cc9a439e7820400567b193e6c4b9a888e5dfa
Pitinat A <pitinat@gmail.com>
2021-06-30 23:02:26 +0700

    Prevent users from downloading files that they do not own

--- app/Http/Controllers/PaperController.php
+++ app/Http/Controllers/PaperController.php
@@ -29,21 +29,26 @@ class PaperController extends Controller
         $paper = Paper::findOrFail($paperId);
         $quizzes = $paper->quiz->all();
 
-        foreach($quizzes as $key=>$quiz){
-            $quiz->quizNumber = $key+1;
-            $quiz->quizString = QuizGenerator::serializedQuestionToString($quiz->question_number,$quiz->question_operator);
+        if(auth()->user()->id == $paper->user_id){
+            foreach($quizzes as $key=>$quiz){
+                $quiz->quizNumber = $key+1;
+                $quiz->quizString = QuizGenerator::serializedQuestionToString($quiz->question_number,$quiz->question_operator);
+            }
+
+            $setting["showAnswer"] = true;
+
+            return response($this->pdf->render('pdf.page', [
+                'paper'=> $paper,
+                'quizzes'=> $quizzes,
+                'setting'=> $setting,
+            ]), 200, [
+                'Content-Type' => 'application/pdf',
+                'Content-Disposition' => ("attachment; filename=question-{$paper->paper_id}.pdf"),
+            ]);
         }
-
-        $setting["showAnswer"] = true;
-
-        return response($this->pdf->render('pdf.page', [
-            'paper'=> $paper,
-            'quizzes'=> $quizzes,
-            'setting'=> $setting,
-        ]), 200, [
-            'Content-Type' => 'application/pdf',
-            'Content-Disposition' => ("attachment; filename=question-{$paper->paper_id}.pdf"),
-        ]);
+        else{
+            abort(403, "You don't have access to this file.");
+        }
 
         // $pdf = PDF::loadView('pdf.page', [
         //         'paper'=> $paper,
