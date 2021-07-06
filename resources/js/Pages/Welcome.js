7957fea6ca1cc04dfdc2ae3b514d27b01a5611df
Pitinat A <pitinat@gmail.com>
2021-07-07 00:23:34 +0700

    Change layout of the welcome page with responsive design

--- resources/js/Pages/Welcome.js
+++ resources/js/Pages/Welcome.js
@@ -23,7 +23,7 @@ export default function Welcome(props) {
                 </div>
             </div>
             <div className="relative flex flex-col flex-wrap items-top justify-evenly content-center min-h-screen bg-white">
-                <div className="relative flex flex-col justify-center w-1/2">
+                <div className="relative flex flex-col self-center w-full p-10 md:w-2/3 md:p-1">
                     <div className="text-3xl text-center mb-5">Why Us?</div>
                     <div className="mb-2">
                         One of the causes of memory loss is Alzheimer's disease. As you get older, your chances of acquiring it increase.
@@ -36,22 +36,62 @@ export default function Welcome(props) {
                     </div>
                 </div>
                 <hr/>
-                <div className="flex flex-col">
-                    <div className="text-3xl text-center mb-28">Our Strong Point</div>
-                    <div className="relative flex justify-around mb-28">
-                        <div className="text-center">Benefit 1</div>
-                        <div className="text-center">Benefit 2</div>
+                <div className="flex flex-col mb-16 md:mb-0 xl:w-3/4">
+                    <div className="text-3xl text-center mb-12 mt-12 md:mt-0">Our Strong Point</div>
+                    <div className="relative flex flex-wrap justify-around mb-10">
+                        <div className="relative flex w-full px-10 mb-10 md:mb-0 md:w-1/2">
+                            <div className="self-center mr-5 p-2 border border-gray-400 rounded-full">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12" fill="none" viewBox="0 0 24 24" stroke="#6B7280">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+                                </svg>
+                            </div>
+                            <div className="relative flex flex-col">
+                                <div className="text-2xl">Benefit 1</div>
+                                <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque ultrices ex, vitae facilisis tellus euismod at. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</div>
+                            </div>
+                        </div>
+                        <div className="relative flex w-full md:w-1/2 px-10">
+                            <div className="self-center mr-5 p-2 border border-gray-400 rounded-full">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12" fill="none" viewBox="0 0 24 24" stroke="#6B7280">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+                                </svg>
+                            </div>
+                            <div className="relative flex flex-col">
+                                <div className="text-2xl">Benefit 2</div>
+                                <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque ultrices ex, vitae facilisis tellus euismod at. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</div>
+                            </div>
+                        </div>
                     </div>
-                    <div className="relative flex justify-around">
-                        <div className="text-center">Benefit 3</div>
-                        <div className="text-center">Benefit 4</div>
+                    <div className="relative flex flex-wrap justify-around">
+                        <div className="relative flex w-full mb-10 md:mb-0 md:w-1/2 px-10">
+                            <div className="self-center mr-5 p-2 border border-gray-400 rounded-full">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12" fill="none" viewBox="0 0 24 24" stroke="#6B7280">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+                                </svg>
+                            </div>
+                            <div className="relative flex flex-col">
+                                <div className="text-2xl">Benefit 3</div>
+                                <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque ultrices ex, vitae facilisis tellus euismod at. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</div>
+                            </div>
+                        </div>
+                        <div className="relative flex w-full md:w-1/2 px-10">
+                            <div className="self-center mr-5 p-2 border border-gray-400 rounded-full">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12" fill="none" viewBox="0 0 24 24" stroke="#6B7280">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+                                </svg>
+                            </div>
+                            <div className="relative flex flex-col">
+                                <div className="text-2xl">Benefit 4</div>
+                                <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque ultrices ex, vitae facilisis tellus euismod at. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</div>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
             </div>
             <div className="relative flex flex-col flex-wrap place-content-center items-center h-32 bg-gray-400">
                 <div>© 2021–2021 Developed by Pitinat A.</div>
-                <div className="pb-2">
+                <div className="pb-2 text-center">
                     Content available under a Creative Commons license.
                 </div>
                 <div>
